651123539ea43003c11a38f36765073361bee943
Tim B. Herbstrith <tim.benedikt.herbstrith@univie.ac.at>
2017-01-14 17:59:26 +0100

    Started setting up the package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='discogs_finder',
+      version='0.1',
+      description='FIND Every Release in your collection',
+      url='https://github.com/tim6her/discogs_finder',
+      author='Tim B. Herbstrith',
+      license='MIT',
+      packages=['discogs_finder'],
+      zip_safe=False)
