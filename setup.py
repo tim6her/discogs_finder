6d1f2edfb8825066e6d5c7bf9231b3afc6361a42
Tim B. Herbstrith <tim.benedikt.herbstrith@univie.ac.at>
2017-01-14 18:22:26 +0100

    Added some description

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='discogs_finder',
       version='0.1',
       description='FIND Every Release in your collection',
+      long_description=readme(),
       url='https://github.com/tim6her/discogs_finder',
       author='Tim B. Herbstrith',
       license='MIT',
       packages=['discogs_finder'],
+      install_requires=[
+                'click', 
+                'urllib2', 
+                'json'
+            ],
       zip_safe=False)
